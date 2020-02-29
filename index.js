deb0a4f5e38dcd6a584d983fec7e8d60cd5c1205
bhuvana.guna <bhuvaneshwari31@gmail.com>
2020-03-01 00:22:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cheerio = require('cheerio');
+var request = require('request');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const constants = {
+    SLASH_TOKEN: process.env.SLASH_TOKEN,
+    BOOKMARK: "/bookmark"
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function(req, res) {
+    //console.log(req.body)
+
+    if(req.body.token == constants.SLASH_TOKEN){
+        let channelId = req.body.channel_id;
+        let query = req.body;
+        let reply = {
+            "response_type": "in_channel",
+            "text" : "Thank you!"
+        }
+        if(req.body.command == constants.BOOKMARK){
+            reply.token = query.token;
+            reply.team_id = query.team_id;
+            reply.channel_id = query.channel_id;
+            reply.channel_name = query.channel_name;
+            reply.timestamp = new Date(query.timestamp);
+            //reply.user_id = query.user_id;
+            //reply.user_name = "bookbot";
+            request(query.text, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                  const $ = cheerio.load(body);
+                  const webpageTitle = $("title").text();
+                  const metaDescription =  $('meta[name=description]').attr("content");
+                  const webpage = {
+                    title: webpageTitle,
+                    metaDescription: metaDescription
+                  }
+
+                    var url = 'https://api.twinword.com/api/v5/topic/generate/';
+                    var headers = { 
+                        'X-Twaip-Key': process.env.TWINWORD_API_KEY,
+                        'Content-Type' : 'application/x-www-form-urlencoded' 
+                    };
+                    var form = { text: metaDescription};
+
+                    request.post({ url: url, form: form, headers: headers }, function (e, r, body) {
+                        // your callback body
+                        console.log(e);
+                        console.log(body)
+                        reply.text = "Your link " + query.text + " has been bookmarked "+ query.user_name;
+                        res.json(reply);
+                    });
+                  
+                  console.log(webpage);
+                }
+              });
+        } else {
+            res.json(reply);
+        }
+    }
+});
+
+app.listen(8080);
+console.log("Server Started")
