34fdce76f4cf06d3ef3fa15630726e4cd0deb701
bhuvana-guna <bhuvaneshwari31@gmail.com>
2020-03-01 18:06:08 +0530

    adding db config

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { Pool, Client } = require('pg')
-const connectionString = process.env.DATABASE_CONNECTION_STRING;
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString: connectionString,
 })
